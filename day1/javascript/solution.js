a1dc6d1a3c787467f2cbdfd0b3398ae631e0cca4
MakeshiftJuliette <71993095+Cinnamonsroll@users.noreply.github.com>
2023-12-01 00:32:48 -0500

    Create solution.js

---
+++ day1/javascript/solution.js
@@ -0,0 +1,10 @@
+const { readFileSync } = require("fs");
+const input = readFileSync("./input.txt", "utf-8");
+const sum = input
+    .split('\n')
+    .map((line) => {
+      const digits = line.match(/\d/g);
+      return digits ? parseInt(digits[0] + digits[digits.length - 1]) : 0;
+    })
+    .reduce((total, value) => total + value, 0);
+console.log(sum);
